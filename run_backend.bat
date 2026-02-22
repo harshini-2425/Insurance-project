867610533fc125928f86b0d6d2c286669b62d696
Teja Harshini Elchuri <elchuritejaharshini@gmail.com>
2026-02-22 14:52:33 +0530

    added new files

--- run_backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\newproject\backend
-set PYTHONPATH=.
-C:\newproject\.venv\Scripts\python.exe -m uvicorn main:app --port 8000 --host 127.0.0.1
--- start_backend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\newproject\backend
-C:\newproject\.venv\Scripts\python.exe -m uvicorn main:app --port 8000 --reload
